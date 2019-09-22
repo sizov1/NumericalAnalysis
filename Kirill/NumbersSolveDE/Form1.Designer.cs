ef52b0874e01a72abf48af6fa39be00e714c61ed
sizov1 <sizow.k.d@gmail.com>
2019-09-22 11:36:36 +0300

    demodemo

---
+++ Kirill/NumbersSolveDE/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace NumbersSolveDE
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using ZedGraph;
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.su0 = new System.Windows.Forms.TextBox();
+            this.sxmax = new System.Windows.Forms.TextBox();
+            this.sh = new System.Windows.Forms.TextBox();
+            this.sN = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(35, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "u0 = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(45, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "h =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(40, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "N = ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(19, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "xmax = ";
+            // 
+            // su0
+            // 
+            this.su0.Location = new System.Drawing.Point(75, 107);
+            this.su0.Name = "su0";
+            this.su0.Size = new System.Drawing.Size(91, 20);
+            this.su0.TabIndex = 5;
+            // 
+            // sxmax
+            // 
+            this.sxmax.Location = new System.Drawing.Point(75, 138);
+            this.sxmax.Name = "sxmax";
+            this.sxmax.Size = new System.Drawing.Size(81, 20);
+            this.sxmax.TabIndex = 6;
+            // 
+            // sh
+            // 
+            this.sh.Location = new System.Drawing.Point(75, 171);
+            this.sh.Name = "sh";
+            this.sh.Size = new System.Drawing.Size(100, 20);
+            this.sh.TabIndex = 7;
+            // 
+            // sN
+            // 
+            this.sN.Location = new System.Drawing.Point(75, 210);
+            this.sN.Name = "sN";
+            this.sN.Size = new System.Drawing.Size(100, 20);
+            this.sN.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::NumbersSolveDE.Properties.Resources.ТестоваяЗадача;
+            this.pictureBox1.Location = new System.Drawing.Point(682, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // zedGraph
+            // 
+            this.zedGraph.AutoScroll = true;
+            this.zedGraph.IsShowPointValues = false;
+            this.zedGraph.Location = new System.Drawing.Point(189, 88);
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.PointValueFormat = "G";
+            this.zedGraph.Size = new System.Drawing.Size(731, 364);
+            this.zedGraph.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 32);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Справка";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(494, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(163, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Без контроля погрешности";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(494, 44);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(159, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.Text = "С контролем погрешности";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(4, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(934, 484);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(926, 458);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Основаная задача 2";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(926, 458);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Основная задача 1";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.zedGraph);
+            this.tabPage2.Controls.Add(this.radioButton1);
+            this.tabPage2.Controls.Add(this.radioButton2);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.sN);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.sh);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.sxmax);
+            this.tabPage2.Controls.Add(this.su0);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(926, 458);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Тестовая задача";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(926, 458);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Таблица";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.dataGridView1.Location = new System.Drawing.Point(4, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(916, 452);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "xi";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "vi";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "v2i";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "vi - v2i";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "ОЛП";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "hi";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "C1";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "C2";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "ui";
+            this.Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "|ui - vi|";
+            this.Column10.Name = "Column10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 529);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Численное решение задачи Коши";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox su0;
+        private System.Windows.Forms.TextBox sxmax;
+        private System.Windows.Forms.TextBox sh;
+        private System.Windows.Forms.TextBox sN;
+        private PictureBox pictureBox1;
+        private ZedGraphControl zedGraph;
+        private Button button2;
+        private HelpProvider helpProvider1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewTextBoxColumn Column6;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
+        private DataGridViewTextBoxColumn Column9;
+        private DataGridViewTextBoxColumn Column10;
+    }
+}
+
---
+++ Kirill/NumbersSolveDE/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Runtime;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace NumbersSolveDE
+{
+    public partial class Form1 : Form
+    {
+
+        const double eqdiff = 1e-15;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form2 q = new Form2();
+            q.Show();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double u0 = System.Convert.ToDouble(su0.Text);
+            double xmax = System.Convert.ToDouble(sxmax.Text);
+            double h = System.Convert.ToDouble(sh.Text);
+            int N = System.Convert.ToInt32(sN.Text);
+            if (radioButton1.Checked) {
+                CalcWithoutControl(u0, xmax, N, h);
+            } else {
+              //  CalcWithControl(u0, xmax, N, h);
+            }
+        }
+
+        double f(double x, double v) { return (-5.0 / 2.0) * v; }
+
+        double twon(int n) { return (double)(2 << (n - 1)); }
+
+        double get_next_value(double x, double v, double h)
+        {
+            double k1 = f(x, v);
+            double k2 = f(x + h / 2, v + k1 * (h / 2));
+            double k3 = f(x + h / 2, v + k2 * (h / 2));
+            double k4 = f(x + h, v + h * k3);
+            return v + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4);
+        }
+
+        void CalcWithoutControl(double u0, double xmax, int N,double h)
+        {
+            ZedGraph.PointPairList f1_list = new ZedGraph.PointPairList();
+            ZedGraph.PointPairList f2_list = new ZedGraph.PointPairList();
+
+            dataGridView1.Rows.Clear();
+            // добавляем начальную точку на график
+            f1_list.Add(0, u0);
+
+            int i = 0;
+            double v = u0;
+            List<List<double>> table = new List<List<double>>();
+
+            for (double x = 0.0; x < xmax && i < N; x += h)
+            {
+
+                // вычисляем следующие значение численной траектории
+                v = get_next_value(x, v, h);
+
+                // вычисляем значение численной траектории с помощью двойного счета с половинным шагом
+                double v2 = get_next_value(x, v, h / 2.0);
+                v2 = get_next_value(x + h / 2.0, v2, h / 2.0);
+
+                // считаем ОЛП
+                double sp = twon(4);
+                double lte = ((v2 - v) / (sp - 1.0)) * sp;
+
+                // вычисляем значение точного решения
+                double u = u0 * Math.Exp( (-5.0/2.0) * (x + h) );
+
+                // добавляем точки точного и численного решения на график 
+                f1_list.Add(x + h, v);
+                f2_list.Add(x + h, u);
+
+                List<double> tablerow = new List<double>(10);
+                tablerow.Add(x + h); tablerow.Add(v); tablerow.Add(v2);
+                tablerow.Add(v - v2); tablerow.Add(lte); tablerow.Add(h);
+                tablerow.Add(0); tablerow.Add(0); tablerow.Add(u);
+                tablerow.Add(Math.Abs(u - v));
+                table.Add(tablerow);
+
+                i++;
+            }
+
+            ZedGraph.GraphPane panel = zedGraph.GraphPane;
+            panel.CurveList.Clear();
+            ZedGraph.LineItem Curve1 = panel.AddCurve("num", f1_list, Color.FromName("Red"), ZedGraph.SymbolType.None);
+            ZedGraph.LineItem Curve2 = panel.AddCurve("exact", f2_list, Color.FromName("Blue"), ZedGraph.SymbolType.None);
+
+            panel.XAxis.Min = -0.1;
+            panel.XAxis.Max = xmax + 0.1;
+            panel.YAxis.Min = -1;
+
+            zedGraph.AxisChange();
+            zedGraph.Invalidate();
+        }
+    }
+
+    
+}
